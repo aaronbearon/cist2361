73f257693a4b1917429e3b1c997a1986661a9ca8
Aaron Blum <googolpup@gmail.com>
2026-04-26 19:59:21 -0400

    Final project in a good state.

--- src/finals/final.cpp
+++ src/finals/final.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cmath>
 #include <fstream>
 #include <iomanip>
@@ -8,10 +9,14 @@
 #include <vector>
 using namespace std;
 
+//// Static data
+
 const int SIZE = 12;
 const string MONTHS[SIZE] = {"January",   "February", "March",    "April",
                              "May",       "June",     "July",     "August",
                              "September", "October",  "November", "December"};
+
+//// Data structures
 
 struct Expense {
   int month;
@@ -26,21 +31,83 @@ struct MonthlySummary {
   double totalBudgeted;
 };
 
-// Function prototypes
+//// Function prototypes
+// Note, prototypes aren't in the same order as definitions.
+
+// File input functions
+vector<Expense> readFile(const string& filename);
 bool parseLine(const string& line, Expense* record);
-void processFile(const string& filename, MonthlySummary summaries[]);
-void printYearlyReport(MonthlySummary summaries[]);
-void sortArray(MonthlySummary summaries[], bool ascending);
-void swapItems(MonthlySummary* a, MonthlySummary* b);
+
+// User input functions
+int inputUserMonth();
+bool isValidInput(string answerStr, int& answer);
+
+// Processing functions
+vector<MonthlySummary> buildMonthlySummaries(const vector<Expense>& expenses);
+
+// Output functions
+void printExpensesByMonth(const vector<Expense>& expenses);
+void printMostExpensiveMonth(const vector<MonthlySummary>& summaries);
+void printMonthlyTotals(const vector<MonthlySummary>& summaries, bool ascending);
+void printMonthReport(int month, const MonthlySummary& summaries, const vector<Expense>& expenses);
+string toCurrency(double amount);
+
+// Sort functions
+bool monthLess(const MonthlySummary* a, const MonthlySummary* b);
+bool monthGreater(const MonthlySummary* a, const MonthlySummary* b);
+bool expenseSort(const Expense& a, const Expense& b);
+
+//// Implementation
 
 int main() {
   const string filename = "expenses.txt";
-  MonthlySummary summaries[SIZE];
+  auto expenses = readFile(filename);  // requirement 1 2
 
-  processFile(filename, summaries);
-  printYearlyReport(summaries);
+  printExpensesByMonth(expenses);  // requirement 3
+
+  auto summaries = buildMonthlySummaries(expenses);
+  printMostExpensiveMonth(summaries);  // requirement 4
+
+  printMonthlyTotals(summaries, false);  // requirement 5
+  printMonthlyTotals(summaries, true);   // requirement 5
+  int month = inputUserMonth();          // requirement 6
+  printMonthReport(month, summaries[month], expenses);    // requirement 6
 
   return 0;
+}
+
+vector<Expense> readFile(const string& filename) {
+  ifstream inputFile(filename);
+  vector<Expense> validRecords;
+
+  if (!inputFile.is_open()) {
+    cout << "Fail: Could not open " << filename << endl;
+    exit(EXIT_FAILURE);
+  }
+
+  string line;
+  while (getline(inputFile, line)) {
+    // If the line is just a trailing newline/empty, we skip it
+    if (line.empty()) continue;
+
+    Expense tempRecord;
+    if (parseLine(line, &tempRecord)) {
+      validRecords.push_back(tempRecord);
+    } else {
+      cout << "Error, invalid line." << endl;
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  inputFile.close();
+
+  // make sure they are sorted
+  sort(validRecords.begin(), validRecords.end(), expenseSort);
+  return validRecords;
+}
+
+bool expenseSort(const Expense& a, const Expense& b) {
+  return a.month < b.month;
 }
 
 /**
@@ -64,13 +131,19 @@ bool parseLine(const string& line, Expense* record) {
 
   try {
     // Attempt conversions - these throw exceptions if they fail
-    record->month = stoi(tokens[0]);
+    record->month = stoi(tokens[0]) - 1;
     record->category = tokens[1];
     record->amount = round(stod(tokens[2]) * 100) / 100.0;
     record->budgeted = round(stod(tokens[3]) * 100) / 100.0;
-    // Negatives aren't allowed
+    // Check for valid values
+    if (record->month < 0 || record->month > 11) {
+      return false;
+    }
     if (record->amount < 0 || record->budgeted < 0) {
       return false;
+    }
+    if (record->category.size() > 12) {
+      record->category.erase(12);
     }
     return true;
   } catch (const invalid_argument& e) {
@@ -80,114 +153,173 @@ bool parseLine(const string& line, Expense* record) {
   }
 }
 
-// Handles file opening and iterates through each line.
-// Load monthly data from the file into the summaries array.
-void processFile(const string& filename, MonthlySummary summaries[]) {
-  ifstream inputFile(filename);
-  vector<Expense> validRecords;
-  for (int i = 0; i < 12; i++) {
-    summaries[i].month = i + 1;
-    summaries[i].totalActual = 0;
-    summaries[i].totalBudgeted = 0;
+void printExpensesByMonth(const vector<Expense>& expenses) {
+  int currentMonth = -1;
+  for (auto expense : expenses) {
+    if (currentMonth != expense.month) {
+      currentMonth = expense.month;
+      cout << endl;
+      cout << MONTHS[expense.month] << endl;
+    }
+
+    cout << fixed << showpoint << setprecision(2);
+    cout << expense.category << " Actual: $" << expense.amount;
+    cout << " Budget: $" << expense.budgeted << endl;
   }
 
-  if (!inputFile.is_open()) {
-    cout << "Fail: Could not open " << filename << endl;
-    return;
+  /*
+
+  January:
+  Mortgage Actual: $1200.45 Budget: $1200
+  Utilities 350 300
+
+  February:
+  Utilities 350 300
+  Groceries 500 450
+
+  */
+}
+
+// Processing
+vector<MonthlySummary> buildMonthlySummaries(const vector<Expense>& expenses) {
+  vector<MonthlySummary> summaries;
+  for (int i = 0; i < SIZE; i++) {
+    MonthlySummary month = {i, 0, 0};
+    summaries.push_back(month);
   }
+  for (auto expense : expenses) {
+    summaries[expense.month].totalActual += expense.amount;
+    summaries[expense.month].totalBudgeted += expense.budgeted;
+  }
+  return summaries;
+}
 
-  string line;
-  while (getline(inputFile, line)) {
-    // If the line is just a trailing newline/empty, we skip it
-    if (line.empty()) continue;
-
-    Expense tempRecord;
-    Expense* ptrRecord = &tempRecord;
-    if (parseLine(line, ptrRecord)) {
-      validRecords.push_back(tempRecord);
-      summaries[tempRecord.month - 1].totalActual += tempRecord.amount;
-      summaries[tempRecord.month - 1].totalBudgeted += tempRecord.budgeted;
-    } else {
-      cout << "Error, invalid line." << endl;
-      exit(EXIT_FAILURE);
+void printMostExpensiveMonth(const vector<MonthlySummary>& summaries) {
+  const MonthlySummary* highest = &summaries[0];
+  for (int i = 0; i < summaries.size(); i++) {
+    const MonthlySummary* summary = &summaries[i];
+    if (summary->totalActual > highest->totalActual) {
+      highest = summary;
     }
   }
 
-  inputFile.close();
+  cout << endl;
+  cout << fixed << showpoint << setprecision(2) << endl;
+  cout << "Month with highest amount spent: " << MONTHS[highest->month] << "." << endl;
+  cout << "Total spent: $" << highest->totalActual << endl;
+  cout << "Total budgeted: $" << highest->totalBudgeted << endl;
+
+  cout << endl;
 }
 
-// Print the data stored in the previously loaded summaries array.
-void printYearlyReport(MonthlySummary summaries[]) {
+void printMonthlyTotals(const vector<MonthlySummary>& summaries, bool ascending) {
+  // Make a copy so we can sort the copy.
+  vector<const MonthlySummary*> index;
+  for (int i = 0; i < summaries.size(); i++) {
+    index.push_back(&summaries[i]);
+  }
+
+  if (ascending) {
+    sort(index.begin(), index.end(), monthLess);
+    cout << "=== Monthly Totals (Ascending) ===" << endl;
+  } else {
+    sort(index.begin(), index.end(), monthGreater);
+    cout << "=== Monthly Totals (Descending) ===" << endl;
+  }
+
+  cout << fixed << showpoint << setprecision(2) << endl;
+  for (auto summary : index) {
+    cout << MONTHS[summary->month] << ": ";
+    cout << "$" << summary->totalActual << endl;
+  }
+  cout << endl;
+}
+
+bool monthLess(const MonthlySummary* a, const MonthlySummary* b) {
+  return a->totalActual < b->totalActual;
+}
+
+bool monthGreater(const MonthlySummary* a, const MonthlySummary* b) {
+  return a->totalActual > b->totalActual;
+}
+
+int inputUserMonth() {
+  while (true) {
+    cout << "Please enter a month number to report: ";
+    int answer = 0;
+    string answerStr;
+    getline(cin, answerStr);
+    if (isValidInput(answerStr, answer)) {
+      return answer - 1; // 0 - 11
+    } else {
+      cout << "Invalid month, try again." << endl;
+    }
+  }
+}
+
+bool isValidInput(string answerStr, int& answer) {
+  try {
+    answer = stoi(answerStr);
+    return answer >= 1 && answer <= 12;  // Month number must be in this range.
+  } catch (const invalid_argument& e) {
+    return false;
+  } catch (const out_of_range& e) {
+    return false;
+  }
+}
+
+void printMonthReport(int month, const MonthlySummary& summaries, const vector<Expense>& expenses) {
+  cout << "=== Expense Report for Month: " << month + 1;
+  cout << " (" << MONTHS[month] << ") ===" << endl;
   cout << fixed << showpoint << setprecision(2);
-  cout << "=== Monthly Totals and Monthly Budgets (Unsorted) ===" << endl;
-  int actualSum = 0;
-  int budgetedSum = 0;
-  for (int i = 0; i < SIZE; i++) {
-    cout << MONTHS[summaries[i].month - 1] << ": ";
-    actualSum += summaries[i].totalActual;
-    budgetedSum += summaries[i].totalBudgeted;
-    cout << "$" << summaries[i].totalActual << ", $"
-         << summaries[i].totalBudgeted << endl;
-  }
+  // int maxCategory = 0;
+  // int maxAmount = 0;
+  // int maxBudget = 0;
+  // for (auto expense : expenses) {
+  //   if (month == expense.month) {
+  //     maxCategory = max((size_t)maxCategory, expense.category.length());
+  //     maxAmount = max((size_t)maxAmount, to_string(expense.amount).length());
+  //   }
+  // }
 
-  cout << endl;
-  cout << "=== Monthly Totals (Ascending) ===" << endl;
-  sortArray(summaries, true);
-  for (int i = 0; i < SIZE; i++) {
-    cout << MONTHS[summaries[i].month - 1] << ": ";
-    cout << "$" << summaries[i].totalActual << endl;
-  }
-
-  cout << endl;
-  cout << "=== Monthly Totals (Descending) ===" << endl;
-  sortArray(summaries, false);
-  for (int i = 0; i < SIZE; i++) {
-    cout << MONTHS[summaries[i].month - 1] << ": ";
-    cout << "$" << summaries[i].totalActual << endl;
-  }
-
-  cout << endl;
-  cout << "Month with highest amount spent: " << MONTHS[summaries[0].month - 1]
-       << ", at $" << summaries[0].totalActual << endl;
-  cout << "Total spent: $" << actualSum << endl;
-  cout << "Total budgeted: $" << budgetedSum << endl;
-
-  cout << endl;
-}
-
-// Use pointers and structs to sort the array.
-void sortArray(MonthlySummary summaries[], bool ascending) {
-  for (int i = 0, j = 0; i < SIZE; i++, j++) {
-    bool swapNeeded = false;
-    // Check that j isn't 0 to prevent -array index out of bounds.
-    if (j > 0) {
-      double* lValPtr = &(summaries[j - 1].totalActual);
-      double* rValPtr = &(summaries[j].totalActual);
-      MonthlySummary* lStructPtr = &(summaries[j - 1]);
-      MonthlySummary* rStructPtr = &(summaries[j]);
-      if ((*lValPtr > *rValPtr && ascending) ||
-          (*lValPtr < *rValPtr && !ascending)) {
-        swapItems(lStructPtr, rStructPtr);
-        swapNeeded = true;
+  for (auto expense : expenses) {
+    if (month == expense.month) {
+      cout << "Category: " << setw(13) << expense.category << " | Spent: ";
+      cout << setw(10) << toCurrency(expense.amount) << " | Budgeted: " << setw(10) << toCurrency(expense.budgeted) << " | ";
+      if (expense.amount > expense.budgeted) {
+        cout << "Over-Budget by ";
+        cout << setw(10) << toCurrency(expense.amount - expense.budgeted) << endl;
+      } else {
+        cout << "Saved          ";
+        cout << setw(10) << toCurrency(expense.budgeted - expense.amount) << endl;
       }
     }
-
-    // Decrement values if a swap had to be performed.
-    // This is a clever sorting algorithm.
-    // It ensures that every new unsorted value is swapped to the left until
-    //  it's in the correct place.
-    if (swapNeeded) {
-      i--;
-      j -= 2;
-    } else {
-      j = i;
-    }
   }
+
+  double subtractBy = min(summaries.totalActual, summaries.totalBudgeted);
+  cout << "Total Over-Budget: $" << summaries.totalActual - subtractBy << endl;
+  cout << "Savings: $" << summaries.totalBudgeted - subtractBy << endl;
 }
 
-// Use pointers to swap structs in the array.
-void swapItems(MonthlySummary* a, MonthlySummary* b) {
-  MonthlySummary temp = *b;
-  *b = *a;
-  *a = temp;
+// // Print the data stored in the previously loaded summaries array.
+// void printYearlyReport(MonthlySummary summaries[]) {
+//   cout << fixed << showpoint << setprecision(2);
+//   cout << "=== Monthly Totals and Monthly Budgets (Unsorted) ===" << endl;
+//   int actualSum = 0;
+//   int budgetedSum = 0;
+//   for (int i = 0; i < SIZE; i++) {
+//     cout << MONTHS[summaries[i].month - 1] << ": ";
+//     actualSum += summaries[i].totalActual;
+//     budgetedSum += summaries[i].totalBudgeted;
+//     cout << "$" << summaries[i].totalActual << ", $"
+//          << summaries[i].totalBudgeted << endl;
+//   }
+
+//   cout << endl;
+// }
+
+string toCurrency(double amount) {
+  stringstream out;
+  out << "$" << fixed << setprecision(2) << amount;
+  return out.str();
 }
